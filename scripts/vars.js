bae550230783e816b631bf0b519bc19d43a74351
maciekbo <maciej.boron.22@gmail.com>
2026-03-29 17:30:14 +0200

    cleaned up vars.js

--- scripts/vars.js
+++ scripts/vars.js
@@ -4,36 +4,24 @@ function set_default(name, val) {
         // 2. Access the result using the same key 'name'
         if (result[name] === undefined) { 
             // If the value is not set, set it.
-            chrome.storage.local.set({[name]: val}, function () {
-                console.log(`Default set for ${name}: ${val}`);
-            });
-        } else {
-            console.log(`Setting ${name} already exists: ${result[name]}`);
+            chrome.storage.local.set({[name]: val});
         }
     })
 }
 
 async function get_val(name){
-    return new Promise(async function (res, rej) {
-      chrome.storage.local.get([name], async function (result) {
-          var userLocal = result[name];
-          res(userLocal);
-      });
-    });
+    const result = await chrome.storage.local.get([name]);
+    return result[name];
 }
 
 set_default("role_exp_val", true);
 
 async function set_updater(name, id) {
-    var checkbox = document.getElementById(id);
+    const checkbox = document.getElementById(id);
     checkbox.checked = await get_val(name);
     checkbox.addEventListener("click", async () => {
         val = checkbox.checked;
-        chrome.storage.local.set({[name]: val}, function() {
-            // Confirmation that the value has been saved
-            console.log(`Setting ${name} updated and saved as: ${val}`);
-        });
-        console.log(val)
+        chrome.storage.local.set({[name]: val});
     })
 }
 
