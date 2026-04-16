c263d124b2f2a710eda0062d69b07e2e8076dc8c
maciekbo <maciej.boron.22@gmail.com>
2026-04-16 19:07:29 +0200

    removed unnesecary comment-code

--- scripts/role_points_calc.js
+++ scripts/role_points_calc.js
@@ -1,18 +1,3 @@
-// just logging the team name for now
-
-// function log_team_name() {
-//   const teamDiv = document.querySelector("div.fantasy-team-teamname-container div.text-ellipsis");
-//   if (teamDiv) {
-//     const teamName = teamDiv.innerHTML;
-//     console.log(teamName);
-//   } else {
-//     // If the element is not found, keep checking every 100ms
-//     setTimeout(log_team_name, 100);
-//   }
-// }
-
-// log_team_name();
-
 async function get_val(name){
     const result = await chrome.storage.local.get([name]);
     return result[name];
